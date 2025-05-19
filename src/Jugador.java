60c318410cc02bf0a0297612ebd4cd651beb9273
Javier Carmona <javiercarmonasolis2006@gmail.com>
2025-05-19 17:42:16 +0200

    Hola señores

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,10 @@
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
 
 public class Jugador {
 
+	public static void InsertarJugadoresBarca(MongoCollection<Document> collection) {
+		
+	}
 }
