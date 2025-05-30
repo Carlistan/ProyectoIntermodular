fd767815af6b34e06847a270db265db9c727de09
Javier Carmona <javiercarmonasolis2006@gmail.com>
2025-05-30 15:28:21 +0200

    Tres en raya

--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -4,6 +4,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class TresEnRaya extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -16,6 +23,19 @@ public class TresEnRaya extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					
+					   String direccion_mongo = "mongodb+srv://javiercarmonasolis2006:2PXjxAw1MGPcNuLy@clusterintermodular.whimpxk.mongodb.net/?retryWrites=true&w=majority&appName=ClusterIntermodular";
+			        	
+			        	MongoClient mongoClient = MongoClients.create(direccion_mongo);
+			            
+			            MongoDatabase database = mongoClient.getDatabase("futbol3raya");
+			            MongoCollection<Document> collectionJugadores = database.getCollection("jugadores");
+			           	            
+			
+					
+					
+					
+					
 					TresEnRaya frame = new TresEnRaya();
 					frame.setVisible(true);
 				} catch (Exception e) {
