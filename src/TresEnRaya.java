fa836595930f4bc497cc6e39b07e478c79f5fb5c
Javier Carmona <javiercarmonasolis2006@gmail.com>
2025-05-30 15:24:33 +0200

    Tres En raya

--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class TresRayas extends JFrame {
+public class TresEnRaya extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -16,7 +16,7 @@ public class TresRayas extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TresRayas frame = new TresRayas();
+					TresEnRaya frame = new TresEnRaya();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -28,7 +28,7 @@ public class TresRayas extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TresRayas() {
+	public TresEnRaya() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
